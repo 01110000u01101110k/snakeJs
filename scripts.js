c0e627e5230095694711c13c6f5063a70a18e805
Dmitry Bondarchuk <dimitrywebdeveloper@gmail.com>
2021-07-08 23:47:02 +0300

    set start functions and some variables

--- scripts.js
+++ scripts.js
@@ -30,6 +30,7 @@ let foodsPositions = [];
 
 let isGameOver = false;
 let isGamePlayed = false;
+let mainAnimationFrame;
 
 let mobile = false;
 if (
@@ -40,16 +41,77 @@ if (
   mobile = true;
 }
 
+const gameOver = () => {};
+
 const getRecordScore = () => {};
 
 const spawnFood = () => {};
 
 const spawnSnake = () => {};
 
-const gameOver = () => {};
+const checkCollisionSnake = () => {};
 
-const mainLoop = () => {};
+const checkCollisionFoodWithSnake = () => {
+  if () {
+    spawnFood();
+  }
+};
 
-const gameStart = () => {};
+const controlSnake = (event) => {
+  if (
+    event.key.toLowerCase() === "a" ||
+    event.key.toLowerCase() === "ф" ||
+    event.key == "ArrowLeft"
+  ) {
+    moveDirection = "left";
+  } else if (
+    event.key.toLowerCase() === "d" ||
+    event.key.toLowerCase() === "в" ||
+    event.key == "ArrowRight"
+  ) {
+    moveDirection = "right";
+  } else if (
+    event.key.toLowerCase() === "w" ||
+    event.key.toLowerCase() === "ц" ||
+    event.key == "ArrowTop"
+  ) {
+    moveDirection = "top";
+  } else if (
+    event.key.toLowerCase() === "s" ||
+    event.key.toLowerCase() === "ы" ||
+    event.key == "ArrowBottom"
+  ) {
+    moveDirection = "bottom";
+  }
+};
 
-const gameLaunch = () => {};
+const moveSnake = () => {
+  let tailSnake = snake.length - 1;
+  snake.pop();
+  snake.push(tailSnake);
+};
+
+const mainLoop = () => {
+  moveSnake();
+  checkCollisionSnake();
+  checkCollisionFoodWithSnake();
+  getRecordScore();
+
+  mainAnimationFrame = requestAnimationFrame(mainLoop);
+};
+
+const gameStart = () => {
+  if (isGamePlayed) {
+    alert("pause");
+    isGamePlayed = false;
+  } else {
+    document.addEventListener("keydown", controlSnake);
+    //document.addEventListener("keyup", controlPlayerRemove);
+    isGamePlayed = true;
+    spawnSnake();
+    spawnFood();
+    mainLoop();
+  }
+};
+
+playingField.addEventListener("onclick", gameStart);
