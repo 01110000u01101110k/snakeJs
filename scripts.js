8c9be0fba33fc030c5f66995ec3e97853022daa7
Dmitry Bondarchuk <dimitrywebdeveloper@gmail.com>
2021-07-09 16:44:15 +0300

    player movement and food spawn

--- scripts.js
+++ scripts.js
@@ -5,25 +5,26 @@ const snake = document.getElementById("snake");
 const score = document.getElementById("score");
 const recordScore = document.getElementById("recordScore");
 
-const playingField = {
+const playingFieldSize = {
   width: 1200,
   height: 550,
 };
 const cellSize = {
-  width: 50,
-  height: 50,
+  width: 35,
+  height: 35,
 };
 const cellForField = {
-  width: playingField.width / cellSize.width,
-  height: playingField.height / cellSize.height,
+  width: playingFieldSize.width / cellSize.width,
+  height: playingFieldSize.height / cellSize.height,
 };
 
-let snake = [];
+let snakeStartSize = 5;
+let snakePiece = [];
 let snakeHeadPosition = {
-  x: null,
-  y: null,
+  x: playingFieldSize.width / 2,
+  y: playingFieldSize.height / 2,
 };
-let moveDirection = null;
+let moveDirection = "left";
 
 let food = [];
 let foodsPositions = [];
@@ -45,59 +46,116 @@ const gameOver = () => {};
 
 const getRecordScore = () => {};
 
-const spawnFood = () => {};
+const randomOffset = () => {
+  let x = Math.floor(playingFieldSize.width * Math.random());
+  let y = Math.floor(playingFieldSize.height * Math.random());
 
-const spawnSnake = () => {};
+  return { x, y };
+};
+
+const spawnFood = () => {
+  let offset = randomOffset();
+  const createFood = document.createElement("div");
+  createFood.classList.add("food");
+  createFood.style.marginTop = `${offset.y}px`;
+  createFood.style.marginLeft = `${offset.x}px`;
+  console.log("spawn food", createFood);
+
+  return playingField.appendChild(createFood);
+};
+
+const spawnSnake = () => {
+  let snakeCellPositionY = snakeHeadPosition.y;
+  let snakeCellPositionX = snakeHeadPosition.x;
+  for (let i = 0; i < snakeStartSize; i++) {
+    const createSnake = document.createElement("div");
+    createSnake.classList.add("snakeCell");
+    createSnake.id = i;
+    /*createSnake.style.marginTop = `${snakeCellPositionY}px`;
+    createSnake.style.marginLeft = `${snakeCellPositionX}px`;*/
+    createSnake.style.transform = `translate(${snakeCellPositionX}px, ${snakeCellPositionY}px)`;
+    snakeCellPositionX += cellSize.width;
+    snakePiece.push(createSnake);
+    console.log("snakePiece", snakePiece);
+
+    playingField.appendChild(createSnake);
+  }
+};
 
 const checkCollisionSnake = () => {};
 
 const checkCollisionFoodWithSnake = () => {
-  if () {
+  /*if () {
     spawnFood();
-  }
+  }*/
 };
 
 const controlSnake = (event) => {
   if (
-    event.key.toLowerCase() === "a" ||
-    event.key.toLowerCase() === "ф" ||
-    event.key == "ArrowLeft"
+    (event.key.toLowerCase() === "a" ||
+      event.key.toLowerCase() === "ф" ||
+      event.key == "ArrowLeft") &&
+    moveDirection !== "right"
   ) {
     moveDirection = "left";
   } else if (
-    event.key.toLowerCase() === "d" ||
-    event.key.toLowerCase() === "в" ||
-    event.key == "ArrowRight"
+    (event.key.toLowerCase() === "d" ||
+      event.key.toLowerCase() === "в" ||
+      event.key == "ArrowRight") &&
+    moveDirection !== "left"
   ) {
     moveDirection = "right";
   } else if (
-    event.key.toLowerCase() === "w" ||
-    event.key.toLowerCase() === "ц" ||
-    event.key == "ArrowTop"
+    (event.key.toLowerCase() === "w" ||
+      event.key.toLowerCase() === "ц" ||
+      event.key == "ArrowTop") &&
+    moveDirection !== "down"
   ) {
     moveDirection = "top";
   } else if (
-    event.key.toLowerCase() === "s" ||
-    event.key.toLowerCase() === "ы" ||
-    event.key == "ArrowBottom"
+    (event.key.toLowerCase() === "s" ||
+      event.key.toLowerCase() === "ы" ||
+      event.key == "ArrowDown") &&
+    moveDirection !== "top"
   ) {
-    moveDirection = "bottom";
+    moveDirection = "down";
   }
 };
 
 const moveSnake = () => {
-  let tailSnake = snake.length - 1;
-  snake.pop();
-  snake.push(tailSnake);
+  let tailSnake = snakePiece[snakePiece.length - 1];
+  if (moveDirection === "left") {
+    tailSnake.style.transform = `translate(${snakeHeadPosition.x}px, ${snakeHeadPosition.y}px)`;
+    snakeHeadPosition.x -= cellSize.width;
+  } else if (moveDirection === "right") {
+    tailSnake.style.transform = `translate(${snakeHeadPosition.x}px, ${snakeHeadPosition.y}px)`;
+    snakeHeadPosition.x += cellSize.width;
+  } else if (moveDirection === "top") {
+    tailSnake.style.transform = `translate(${snakeHeadPosition.x}px, ${snakeHeadPosition.y}px)`;
+    snakeHeadPosition.y -= cellSize.width;
+  } else {
+    tailSnake.style.transform = `translate(${snakeHeadPosition.x}px, ${snakeHeadPosition.y}px)`;
+    snakeHeadPosition.y += cellSize.width;
+  }
+  snakePiece.pop();
+  snakePiece.unshift(tailSnake);
+  checkCollisionFoodWithSnake();
+
+  console.log("snakePiece", moveDirection);
+  /*let tailSnake = snakePiece[snakePiece.length - 1];
+  snakePiece[0].marginLeft += cellSize.width * snakePiece.length;
+  snakePiece.push(tailSnake);
+  snakePiece.pop();*/
+};
+
+const changeFrame = () => {
+  moveSnake();
+  checkCollisionSnake();
+  getRecordScore();
 };
 
 const mainLoop = () => {
-  moveSnake();
-  checkCollisionSnake();
-  checkCollisionFoodWithSnake();
-  getRecordScore();
-
-  mainAnimationFrame = requestAnimationFrame(mainLoop);
+  mainAnimationFrame = setInterval(changeFrame, 300);
 };
 
 const gameStart = () => {
@@ -111,7 +169,9 @@ const gameStart = () => {
     spawnSnake();
     spawnFood();
     mainLoop();
+    console.log("data");
   }
 };
 
-playingField.addEventListener("onclick", gameStart);
+startBtn.addEventListener("click", gameStart);
+playingField.addEventListener("click", gameStart);
